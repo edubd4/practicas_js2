33d856441d63ade99b6627e195cd4a07231b913b
edub <eduardo.barreiro93@gmail.com>
2022-05-20 18:58:31 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let edad = parseInt(prompt('Ingrese su edad'));
+
+if (edad >= 18){
+    document.write('Usted ya puede conducir');
+} else {
+    document.write('lo siento, todavia eres menor de edad');
+}
